63c5789998f7c42e0cfee90c40e96635d76787c5
Alexander Mayr <alex.mayr.mobil@gmail.com>
2025-02-23 13:12:33 +0100

    updated config.py to read multiple .env files -> multiple docker compose

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# config.py
 import os
 
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(dotenv_path=".env")
+load_dotenv(dotenv_path="postgres/db.env")
 
 # Configuration variables
 LAKE_TYPE = os.getenv("LAKE_TYPE", "parquet")
@@ -14,9 +14,8 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "csv_deltas")
 PROCESSING_MODE = os.getenv("PROCESSING_MODE", "stream")
 
-# PostgreSQL connection details (without JDBC)
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "host.docker.internal")
 POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
-POSTGRES_DB = os.getenv("POSTGRES_DB", "datalake")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
