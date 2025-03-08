46567ca87935eea5bc509faddfa95a791e5aad36
Alexander <alex.mayr.mobil@gmail.com>
2025-03-08 09:07:16 +0100

    updated kafka streamer to use confluent_kafka

--- kafka_streamer.py
+++ kafka_streamer.py
@@ -1,8 +1,10 @@
 # kafka_streamer.py
 import json
 import os
+from sys import api_version
 
 import pandas as pd
+from confluent_kafka import Producer
 from logger import log
 
 from config import KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC
@@ -10,8 +12,17 @@ from kafka import KafkaProducer
 
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    #bootstrap_servers=['localhost:9092'],
+    api_version=(0, 10),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
+
+producer1 = Producer(
+        {'bootstrap.servers': 'localhost:9092',
+         'compression.type': 'lz4'
+         }
+)
+
 
 
 def stream_delta(file_path):
@@ -25,8 +36,9 @@ def stream_delta(file_path):
             "filename": os.path.basename(file_path).replace(".csv", ""),
             "records": records
         }
-        producer.send(KAFKA_TOPIC, value=message)
-        producer.flush()
+        producer1.produce(KAFKA_TOPIC, key=message["filename"], value=json.dumps(message["records"]))
+        log.debug(f"Produced message to Kafka topic '{KAFKA_TOPIC}': {message['filename']}, {records}")
+        producer1.flush()
         log.info(f"Streamed data from {file_path} to Kafka topic '{KAFKA_TOPIC}' with {len(records)} records.")
     except Exception as e:
         log.error(f"Error streaming data from {file_path}: {e}")
