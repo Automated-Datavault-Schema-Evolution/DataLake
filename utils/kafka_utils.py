519fcfc2683c89bece70c97f5704ed23ea4dac2d
Alexander Mayr <alex.mayr.mobil@gmail.com>
2025-05-30 11:15:41 +0200

    fixed error regarding topic not found

--- utils/kafka_utils.py
+++ utils/kafka_utils.py
@@ -7,11 +7,11 @@ from config import KAFKA_TOPIC, KAFKA_BOOTSTRAP_SERVERS
 
 def get_kafka_consumer(group_id="delta-bulk"):
     consumer = KafkaConsumer(
+        KAFKA_TOPIC,
         group_id=group_id,
         bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
         auto_offset_reset='earliest',
         enable_auto_commit=False,
-        topic=KAFKA_TOPIC,
         value_deserializer=lambda value: json.loads(value.decode('utf-8'))
     )
     return consumer
