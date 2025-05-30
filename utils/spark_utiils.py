3c627e955a3ad811be6ccfe0c607c0b46ff5fc4a
Alexander Mayr <alex.mayr.mobil@gmail.com>
2025-05-30 11:36:27 +0200

    fixed versions for kafka spark

--- utils/spark_utiils.py
+++ utils/spark_utiils.py
@@ -10,8 +10,12 @@ def get_spark_session(app_name="Kafka_Consumer_Lake_Handler"):
         SparkSession.builder
         .appName(app_name)
         .master(spark_master)
-        .config("spark.jars.packages",
-                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2,org.apache.kafka:kafka-clients:3.3.2")
+        .config(
+            "spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,"
+            "org.apache.kafka:kafka-clients:3.5.0,"
+            "io.delta:delta-spark_2.12:3.0.0"
+        )
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     )
